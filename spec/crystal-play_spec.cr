1ff9483e760509976990fe23cd576c22a5ce66b0
Stephen Dolan <dolan.stephen1@gmail.com>
2021-06-04 12:57:36 -0400

    Initial commit

---
+++ spec/crystal-play_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Crystal::Play do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/crystal-play.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `Crystal::Play`
+module Crystal::Play
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
